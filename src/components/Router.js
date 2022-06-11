ae3372911f75fe2f9439702a3b53c84e089c8de9
jeehye03 <ks6247@naver.com>
2022-06-11 16:13:12 +0900

    first commit

--- src/components/Router.js
+++ src/components/Router.js
@@ -2,14 +2,16 @@ import React from "react"
 import { Route, Routes } from "react-router-dom";
 import Home from "../routes/Home";
 import Login from "../routes/Login";
+import Signup from "../routes/Signup";
 
 
 const AppRouter = () => {
     return(
         <>
             <Routes>
-                <Route path="/" element={<Home/>}/>
-                <Route path="login" element={<Login/>}/>
+                <Route path="/*" element={<Home/>}/>
+                <Route path="user/login" element={<Login/>}/>
+                <Route path="user/signup" element={<Signup/>}/>
             </Routes>
         </>
     )

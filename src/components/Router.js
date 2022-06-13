81f7e9623de4dbf0dc62a2e620d52ff59804869b
natural-nine <wan.jun.bruce@gmail.com>
2022-06-13 20:15:25 +0900

    second

--- src/components/Router.js
+++ src/components/Router.js
@@ -1,5 +1,6 @@
 import React from "react"
 import { Route, Routes } from "react-router-dom";
+import Detail from "../routes/Detail";
 import Home from "../routes/Home";
 import Login from "../routes/Login";
 import Upload from "../routes/Upload";
@@ -8,11 +9,14 @@ import Upload from "../routes/Upload";
 const AppRouter = () => {
     return(
         <>
+        
             <Routes>
                 <Route path="/" element={<Home/>}/>
                 <Route path="login" element={<Login/>}/>
                 <Route path="/upload" element={<Upload/>}/>
+                <Route path="/detail/:id" element={<Detail/>}/>
             </Routes>
+        
         </>
     )
 }
